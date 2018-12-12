f3fdb49ff64b68b4d78c0a7c21960f01ac0b8e4c
sbenard <sbenard@mtwin.fr>
2018-12-12 23:45:31 +0100

    Shorter experimental redist

--- makeRedist.bat
+++ makeRedist.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set redist=redist
-set game=MyGame
+set game=CatsAreAssholes
 
 echo Cleaning...
 rmdir /Q/S "%redist%" >nul
@@ -10,37 +10,28 @@ mkdir "%redist%\%game%\" >nul
 
 echo Copying hl.exe files...
 copy %haxepath%\hl.exe "%redist%\%game%"
-
-copy %haxepath%\chroma.hdll "%redist%\%game%"
-copy %haxepath%\directx.hdll "%redist%\%game%"
-copy %haxepath%\fmt.hdll "%redist%\%game%"
-copy %haxepath%\freetype.hdll "%redist%\%game%"
-copy %haxepath%\openal.hdll "%redist%\%game%"
-copy %haxepath%\sdl.hdll "%redist%\%game%"
-copy %haxepath%\sqlite.hdll "%redist%\%game%"
-copy %haxepath%\ssl.hdll "%redist%\%game%"
-copy %haxepath%\ui.hdll "%redist%\%game%"
-copy %haxepath%\uv.hdll "%redist%\%game%"
-
-copy %haxepath%\depends.dll "%redist%\%game%"
-copy %haxepath%\gc.dll "%redist%\%game%"
-copy %haxepath%\libcurl.dll "%redist%\%game%"
 copy %haxepath%\libhl.dll "%redist%\%game%"
-copy %haxepath%\libpcre-1.dll "%redist%\%game%"
-copy %haxepath%\msvcp120.dll "%redist%\%game%"
-copy %haxepath%\msvcr100.dll "%redist%\%game%"
 copy %haxepath%\msvcr120.dll "%redist%\%game%"
-copy %haxepath%\neko.dll "%redist%\%game%"
 copy %haxepath%\OpenAL32.dll "%redist%\%game%"
 copy %haxepath%\SDL2.dll "%redist%\%game%"
-copy %haxepath%\zlib1.dll "%redist%\%game%"
+REM copy %haxepath%\depends.dll "%redist%\%game%"
+REM copy %haxepath%\gc.dll "%redist%\%game%"
+REM copy %haxepath%\msvcr100.dll "%redist%\%game%"
+REM copy %haxepath%\msvcp120.dll "%redist%\%game%"
+REM copy %haxepath%\neko.dll "%redist%\%game%"
 
-copy %haxepath%\ssl.ndll "%redist%\%game%"
-copy %haxepath%\std.ndll "%redist%\%game%"
-copy %haxepath%\zlib.ndll "%redist%\%game%"
-copy %haxepath%\regexp.ndll "%redist%\%game%"
-copy %haxepath%\mysql5.ndll "%redist%\%game%"
-copy %haxepath%\mod_tora2.ndll "%redist%\%game%"
+copy %haxepath%\fmt.hdll "%redist%\%game%"
+copy %haxepath%\openal.hdll "%redist%\%game%"
+copy %haxepath%\ui.hdll "%redist%\%game%"
+copy %haxepath%\uv.hdll "%redist%\%game%"
+copy %haxepath%\sdl.hdll "%redist%\%game%"
+REM copy %haxepath%\ssl.ndll "%redist%\%game%"
+REM copy %haxepath%\std.ndll "%redist%\%game%"
+REM copy %haxepath%\zlib.ndll "%redist%\%game%"
+REM copy %haxepath%\regexp.ndll "%redist%\%game%"
+REM copy %haxepath%\mysql5.ndll "%redist%\%game%"
+REM copy %haxepath%\mod_tora2.ndll "%redist%\%game%"
+
 
 ren "%redist%\%game%"\*.exe "%game%.exe"
 
