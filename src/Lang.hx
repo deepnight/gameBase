f311bfbe83d12cd62e5d0bdd48578fc5a74a58dc
deepnight <fork@deepnight.net>
2019-01-31 09:50:58 +0100

    Lang init flag

--- src/Lang.hx
+++ src/Lang.hx
@@ -2,12 +2,16 @@ import mt.data.GetText;
 
 class Lang {
     public static var CUR = "??";
+    static var initDone = false;
 
     public static function init(lid:String) {
         CUR = lid;
+        initDone = true;
     }
 
     public static function untranslated(str:Dynamic) : LocaleString {
+        if( !initDone )
+            throw "Lang.init() required.";
         return cast str;
     }
 }
