946e52a5f0512daf0d38857a7e0089f062110dbd
Sébastien Bénard <fork@deepnight.net>
2019-09-25 14:54:26 +0200

    Minimal TilEd level

---
+++ res/map/markers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="markers" tilewidth="16" tileheight="16" tilecount="64" columns="16">
+ <image source="markers.png" width="256" height="64"/>
+</tileset>
--- src/Level.hx
+++ src/Level.hx
@@ -2,18 +2,17 @@ class Level extends dn.Process {
 	public var game(get,never) : Game; inline function get_game() return Game.ME;
 	public var fx(get,never) : Fx; inline function get_fx() return Game.ME.fx;
 
-	public var wid : Int;
-	public var hei : Int;
+	public var wid(get,never) : Int; inline function get_wid() return tmap.wid;
+	public var hei(get,never) : Int; inline function get_hei() return tmap.hei;
+	var tmap : h2dTiled.TiledMap;
 
 	var invalidated = true;
 
 	public function new() {
 		super(Game.ME);
-
 		createRootInLayers(Game.ME.scroller, Const.DP_BG);
 
-		wid = 16;
-		hei = 16;
+		tmap = new h2dTiled.TiledMap(hxd.Res.map.level);
 	}
 
 	public inline function isValid(cx,cy) return cx>=0 && cx<wid && cy>=0 && cy<hei;
