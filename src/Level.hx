84316d1cf6e426f2eda1a679f2ae69c1bd4b71bb
Sébastien Bénard <fork@deepnight.net>
2019-10-03 13:53:28 +0200

    Removed Tiled from code

--- src/Level.hx
+++ src/Level.hx
@@ -2,17 +2,14 @@ class Level extends dn.Process {
 	public var game(get,never) : Game; inline function get_game() return Game.ME;
 	public var fx(get,never) : Fx; inline function get_fx() return Game.ME.fx;
 
-	public var wid(get,never) : Int; inline function get_wid() return tmap.wid;
-	public var hei(get,never) : Int; inline function get_hei() return tmap.hei;
-	var tmap : tiled.TMap;
+	public var wid(get,never) : Int; inline function get_wid() return 16;
+	public var hei(get,never) : Int; inline function get_hei() return 16;
 
 	var invalidated = true;
 
 	public function new() {
 		super(Game.ME);
 		createRootInLayers(Game.ME.scroller, Const.DP_BG);
-
-		tmap = new tiled.TMap(hxd.Res.map.level);
 	}
 
 	public inline function isValid(cx,cy) return cx>=0 && cx<wid && cy>=0 && cy<hei;
