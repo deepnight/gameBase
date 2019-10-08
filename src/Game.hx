0d81f3cb25eb310b2fede45ad8a0a9da7f176dc5
Sébastien Bénard <fork@deepnight.net>
2019-10-08 12:28:52 +0200

    Fixed useless double filter for pixel-scaling

--- src/Game.hx
+++ src/Game.hx
@@ -19,8 +19,6 @@ class Game extends Process {
 		createRootInLayers(Main.ME.root, Const.DP_BG);
 
 		scroller = new h2d.Layers();
-		if( Const.SCALE>1 )
-			scroller.filter = new h2d.filter.ColorMatrix(); // force pixel render
 		root.add(scroller, Const.DP_BG);
 
 		camera = new Camera();
