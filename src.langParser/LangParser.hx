671051962ba860de0d5d3be72494b377d58af1f2
Sébastien Bénard <fork@deepnight.net>
2019-10-22 10:09:05 +0200

    Auto detect CDB files

--- src.langParser/LangParser.hx
+++ src.langParser/LangParser.hx
@@ -4,13 +4,27 @@ class LangParser {
 	public static function main() {
 		var name = "sourceTexts";
 		Sys.println("Building "+name+" file...");
+		var cdbs = findAll("res", "cdb");
 		var data = GetText.doParseGlobal({
 			codePath: "src",
 			codeIgnore: null,
-			cdbFiles: [],
+			cdbFiles: cdbs,
 			cdbSpecialId: [],
 			potFile: "res/lang/"+name+".pot",
 		});
 		Sys.println("Done.");
+	}
+
+	static function findAll(path:String, ext:String, ?cur:Array<String>) {
+		var ext = "."+ext;
+		var all = cur==null ? [] : cur;
+		for(e in sys.FileSystem.readDirectory(path)) {
+			e = path+"/"+e;
+			if( e.indexOf(ext)>=0 && e.lastIndexOf(ext)==e.length-ext.length )
+				all.push(e);
+			if( sys.FileSystem.isDirectory(e) && e.indexOf(".tmp")<0 )
+				findAll(e, ext, all);
+		}
+		return all;
 	}
 }
