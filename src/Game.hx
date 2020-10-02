780db21030639113d32c40a49eb0270185a883d9
Sébastien Bénard <github@deepnight.net>
2020-10-02 09:33:30 +0200

    Added more comments

--- src/Game.hx
+++ src/Game.hx
@@ -4,15 +4,28 @@ import hxd.Key;
 class Game extends Process {
 	public static var ME : Game;
 
+	/** Game controller (pad or keyboard) **/
 	public var ca : dn.heaps.Controller.ControllerAccess;
+
+	/** Particles **/
 	public var fx : Fx;
+
+	/** Basic viewport control **/
 	public var camera : Camera;
+
+	/** Container of all visual game objects. Ths wrapper is moved around by Camera. **/
 	public var scroller : h2d.Layers;
+
+	/** Level data **/
 	public var level : Level;
+
+	/** UI **/
 	public var hud : ui.Hud;
 
+	/** Slow mo internal values**/
 	var curGameSpeed = 1.0;
 	var slowMos : Map<String, { id:String, t:Float, f:Float }> = new Map();
+
 
 	public function new() {
 		super(Main.ME);
