299ba220ea135b29dbf39f5f763cead737dddabb
Sébastien Bénard <github@deepnight.net>
2020-12-14 13:25:17 +0100

    Import cleanup

--- src/import.hx
+++ src/import.hx
@@ -1,11 +1,13 @@
 import dn.M;
 import dn.Lib;
 import dn.Color;
-import dn.Color as C;
-import hxd.Key as K;
 import dn.Tweenie;
 import dn.data.GetText;
 import dn.heaps.slib.*;
 import ui.Console;
 import tools.*;
+
+// Aliases
+import dn.Color as C;
+import hxd.Key as K;
 import tools.LPoint as P;
