6976aaa23fabb2b8b379a507b2bc3f621caee4fa
Sébastien Bénard <github@deepnight.net>
2021-03-02 16:21:52 +0100

    Updated imports

--- src/import.hx
+++ src/import.hx
@@ -1,12 +1,19 @@
+// Libs
 import dn.M;
 import dn.Lib;
 import dn.Color;
 import dn.Tweenie;
 import dn.data.GetText;
+import dn.heaps.Controller;
 import dn.heaps.slib.*;
+
+// Project classes
 import ui.Console;
 import tools.*;
+import en.*;
 import Types;
+
+// LDtk
 import World;
 
 // Aliases
