fca4687bb857b4455af811aec78d005f5785e3c7
Sébastien Bénard <github@deepnight.net>
2021-03-02 14:41:37 +0100

    Added debug to HUD

--- src/ui/Hud.hx
+++ src/ui/Hud.hx
@@ -8,6 +8,8 @@ class Hud extends dn.Process {
 	var flow : h2d.Flow;
 	var invalidated = true;
 
+	var debugText : h2d.Text;
+
 	public function new() {
 		super(Game.ME);
 
@@ -15,11 +17,27 @@ class Hud extends dn.Process {
 		root.filter = new h2d.filter.Nothing(); // force pixel perfect rendering
 
 		flow = new h2d.Flow(root);
+
+		debugText = new h2d.Text(Assets.fontSmall, root);
+		clearDebug();
 	}
 
 	override function onResize() {
 		super.onResize();
 		root.setScale(Const.UI_SCALE);
+	}
+
+	public inline function clearDebug() {
+		debugText.text = "";
+		debugText.visible = false;
+	}
+	public inline function debug(v:Dynamic, clear=true) {
+		if( clear )
+			debugText.text = Std.string(v);
+		else
+			debugText.text += "\n"+v;
+		debugText.visible = true;
+		debugText.x = Std.int( w()/Const.UI_SCALE - 4 - debugText.textWidth );
 	}
 
 	public inline function invalidate() invalidated = true;
