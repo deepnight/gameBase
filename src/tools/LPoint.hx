f2f6313d9c7745e12636afffa770dd97fa27dc70
Sébastien Benard <github@deepnight.net>
2021-04-05 14:27:51 +0200

    Cleanup LPoint

--- src/tools/LPoint.hx
+++ src/tools/LPoint.hx
@@ -118,26 +118,27 @@ class LPoint {
 	}
 
 	/** Return distance to something else, in grid unit **/
-	public inline function distCase(?e:Entity, ?pt:LPoint, ?cx=0, ?cy=0, ?xr=0.5, ?yr=0.5) {
+	public inline function distCase(?e:Entity, ?pt:LPoint, ?tcx=0, ?tcy=0, ?txr=0.5, ?tyr=0.5) {
 		if( e!=null )
 			return M.dist(this.cx+this.xr, this.cy+this.yr, e.cx+e.xr, e.cy+e.yr);
 		else if( pt!=null )
 			return M.dist(this.cx+this.xr, this.cy+this.yr, pt.cx+pt.xr, pt.cy+pt.yr);
 		else
-			return M.dist(this.cx+this.xr, this.cy+this.yr, cx+xr, cy+yr);
+			return M.dist(this.cx+this.xr, this.cy+this.yr, tcx+txr, tcy+tyr);
 	}
 
-	/** Return distance to something else, in level pixels **/
-	public inline function distPx(?e:Entity, ?pt:LPoint, ?x=0., ?y=0.) {
+	/** Distance to something else, in level pixels **/
+	public inline function distPx(?e:Entity, ?pt:LPoint, ?lvlX=0., ?lvlY=0.) {
 		if( e!=null )
 			return M.dist(levelX, levelY, e.attachX, e.attachY);
 		else if( pt!=null )
 			return M.dist(levelX, levelY, pt.levelX, pt.levelY);
 		else
-			return M.dist(levelX, levelY, x, y);
+			return M.dist(levelX, levelY, lvlX, lvlY);
 	}
-	/** Return distance to something else, in level pixels **/
-	public inline function angTo(?e:Entity, ?pt:LPoint, ?x=0., ?y=0.) {
+
+	/** Angle in radians to something else, in level pixels **/
+	public inline function angTo(?e:Entity, ?pt:LPoint, ?lvlX=0., ?lvlY=0.) {
 		if( e!=null )
 			return Math.atan2((e.cy+e.yr)-cyf, (e.cx+e.xr)-cxf );
 		else if( pt!=null )
