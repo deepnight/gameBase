6e4525b2fe90d5ffaad1e885a378ccce3374ca59
Sébastien Benard <github@deepnight.net>
2021-04-07 18:09:19 +0200

    Added app entity field

--- src/gameBase/gm/Entity.hx
+++ src/gameBase/gm/Entity.hx
@@ -5,6 +5,7 @@ class Entity {
     public static var GC : Array<Entity> = [];
 
 	// Various getters to access all important stuff easily
+	public var app(get,never) : App; inline function get_app() return App.ME;
 	public var game(get,never) : Game; inline function get_game() return Game.ME;
 	public var fx(get,never) : Fx; inline function get_fx() return Game.ME.fx;
 	public var level(get,never) : Level; inline function get_level() return Game.ME.level;
