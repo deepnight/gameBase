694c514d833057ef25e5dcb16872a82eb35dd930
Sébastien Benard <github@deepnight.net>
2021-04-07 18:09:12 +0200

    Added comments

--- src/gameBase/sample/SampleGame.hx
+++ src/gameBase/sample/SampleGame.hx
@@ -1,10 +1,12 @@
 package sample;
 
+/**
+	This small class just creates a SamplePlayer instance in current level
+**/
 class SampleGame extends Game {
 	public function new() {
 		super();
-		
+
 		new SamplePlayer();
 	}
-
 }
