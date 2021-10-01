bbdb43caa24564683ff5d62cd5b83b2d762d2e03
Sebastien Benard <github@deepnight.net>
2021-10-01 21:27:04 +0200

    Updated to latest GetText

--- src/langParser/LangParser.hx
+++ src/langParser/LangParser.hx
@@ -2,38 +2,19 @@ import dn.data.GetText;
 
 class LangParser {
 	public static function main() {
-		var name = "sourceTexts";
-		Sys.println("Building "+name+" file...");
-		var cdbs = findAll("res", "cdb");
-		try {
-			var data = GetText.doParseGlobal({
-				codePath: "src",
-				codeIgnore: null,
-				cdbFiles: cdbs,
-				cdbSpecialId: [],
-				potFile: "res/lang/"+name+".pot",
-			});
-		}
-		catch(e:String) {
-			Sys.println("");
-			Sys.println(e);
-			Sys.println("Extraction failed: fatal error!");
-			Sys.println("");
-			Sys.exit(1);
-		}
-		Sys.println("Done.");
-	}
+		// Extract from source code
+		var all = GetText.parseSourceCode("src");
 
-	static function findAll(path:String, ext:String, ?cur:Array<String>) {
-		var ext = "."+ext;
-		var all = cur==null ? [] : cur;
-		for(e in sys.FileSystem.readDirectory(path)) {
-			e = path+"/"+e;
-			if( e.indexOf(ext)>=0 && e.lastIndexOf(ext)==e.length-ext.length )
-				all.push(e);
-			if( sys.FileSystem.isDirectory(e) && e.indexOf(".tmp")<0 )
-				findAll(e, ext, all);
-		}
-		return all;
+		// Extract from LDtk
+		all = all.concat( GetText.parseLdtk("res/levels/sampleWorld.ldtk", {
+			entityFields: [], // fill this with Entity fields that should be extracted for localization
+			levelFieldIds: [], // fill this with Level fields that should be extracted for localization
+		}));
+
+		// Extract from CastleDB
+		all = all.concat( GetText.parseCastleDB("res/data.cdb") );
+
+		// Write POT
+		GetText.writePOT("res/lang/sourceTexts.pot",all);
 	}
 }
