a72425a596697fd9eebbd2585ef002fc56b71b25
Sebastien Benard <github@deepnight.net>
2022-07-19 18:42:12 +0200

    Added Lang.getSystemLang()

--- src/game/assets/Lang.hx
+++ src/game/assets/Lang.hx
@@ -4,7 +4,6 @@ import dn.data.GetText;
 
 class Lang {
     static var _initDone = false;
-    static var DEFAULT = "en";
     public static var CUR = "??";
     public static var t : GetText;
 
@@ -13,14 +12,36 @@ class Lang {
             return;
 
         _initDone = true;
-        CUR = lid==null ? DEFAULT : lid;
+        CUR = lid==null ? getSystemLang() : lid;
+        var res =
+            try hxd.Res.load("lang/"+CUR+".po")
+            catch(_) {
+                CUR = "en";
+                hxd.Res.load("lang/"+CUR+".po");
+            }
 
 		t = new GetText();
-		t.readPo( hxd.Res.load("lang/"+CUR+".po").entry.getBytes() );
+		t.readPo( res.entry.getBytes() );
     }
 
     public static function untranslated(str:Dynamic) : LocaleString {
         init();
         return t.untranslated(str);
     }
+
+
+    /**
+        Return a simple language code, depending on current System setting (eg. "en", "fr", "de" etc.). If something goes wrong, this returns "en".
+        See: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    **/
+	public static function getSystemLang() : String {
+		try {
+			var code = hxd.System.getLocale();
+			if( code.indexOf("-")>=0 )
+				code = code.substr(0,code.indexOf("-") );
+			return code.toLowerCase();
+		}
+		catch(_)
+            return "en";
+	}
 }
