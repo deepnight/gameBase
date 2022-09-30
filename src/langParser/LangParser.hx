d091a51f4a08a8e0580d55a78fbce6ad8619f48c
Sebastien Benard <github@deepnight.net>
2022-09-30 16:34:20 +0200

    Simpler LangParser

--- src/langParser/LangParser.hx
+++ src/langParser/LangParser.hx
@@ -2,20 +2,22 @@ import dn.data.GetText;
 
 class LangParser {
 	public static function main() {
+		var allEntries : Array<PoEntry> = [];
+
 		// Extract from source code
-		var all = GetText.parseSourceCode("src");
+		GetText.parseSourceCode(allEntries, "src");
 
 		// Extract from LDtk
-		all = all.concat( GetText.parseLdtk("res/levels/sampleWorld.ldtk", {
+		GetText.parseLdtk(allEntries, "res/levels/sampleWorld.ldtk", {
 			entityFields: [], // fill this with Entity fields that should be extracted for localization
 			levelFieldIds: [], // fill this with Level fields that should be extracted for localization
-		}));
+		});
 
 		// Extract from CastleDB
-		all = all.concat( GetText.parseCastleDB("res/data.cdb") );
+		GetText.parseCastleDB(allEntries, "res/data.cdb");
 
 		// Write POT
-		GetText.writePOT("res/lang/sourceTexts.pot",all);
+		GetText.writePOT("res/lang/sourceTexts.pot", allEntries);
 
 		Sys.println("Done.");
 	}
