8d491a30f3c9bcba0b554138c3b9421483828377
Sebastien Benard <github@deepnight.net>
2022-10-03 22:11:20 +0200

    Adjusted game scaling

--- src/game/Const.hx
+++ src/game/Const.hx
@@ -27,7 +27,7 @@ class Const {
 	public static var SCALE(get,never) : Int;
 		static inline function get_SCALE() {
 			// can be replaced with another way to determine the game scaling
-			return dn.heaps.Scaler.bestFit_i(170,140);
+			return dn.heaps.Scaler.bestFit_i(220,190);
 		}
 
 	/** Specific scaling for top UI elements **/
