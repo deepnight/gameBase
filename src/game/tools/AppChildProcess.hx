00590a36e9d8b83af5af25b771368f7123470f07
Sebastien Benard <github@deepnight.net>
2022-10-03 21:32:26 +0200

    Added AppChild fade in/out

--- src/game/tools/AppChildProcess.hx
+++ src/game/tools/AppChildProcess.hx
@@ -5,14 +5,42 @@ class AppChildProcess extends dn.Process {
 
 	public var app(get,never) : App; inline function get_app() return App.ME;
 
+	var fadeMask : h2d.Bitmap;
+
 	public function new() {
 		super(App.ME);
 		createRootInLayers(App.ME.root, Const.DP_MAIN);
 		ALL.push(this);
+
+		fadeMask = new h2d.Bitmap( h2d.Tile.fromColor(Assets.walls()) );
+		root.add(fadeMask, 99999);
+		fadeMask.visible = false;
+	}
+
+	function fadeIn(t=1.5, ?then:Void->Void) {
+		root.over(fadeMask);
+		fadeMask.visible = true;
+		tw.createS(fadeMask.alpha, 1>0, t).end( ()->{
+			fadeMask.visible = false;
+			if( then!=null )
+				then();
+		});
+	}
+	function fadeOut(t=0.5, ?then:Void->Void) {
+		root.over(fadeMask);
+		fadeMask.visible = true;
+		tw.createS(fadeMask.alpha, 0>1, t).end( then );
 	}
 
 	override function onDispose() {
 		super.onDispose();
 		ALL.remove(this);
+	}
+
+	override function onResize() {
+		super.onResize();
+
+		fadeMask.scaleX = w();
+		fadeMask.scaleY = h();
 	}
 }
