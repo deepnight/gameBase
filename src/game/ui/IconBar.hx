80fa0b18a3e3bebbf774b1232630975600ff82bb
Sebastien Benard <github@deepnight.net>
2022-10-12 15:52:10 +0200

    Added IconBar UI (#85)

---
+++ src/game/ui/IconBar.hx
@@ -0,0 +1,27 @@
+package ui;
+
+class IconBar extends h2d.TileGroup {
+	var curX = 0;
+	public var width(default,null) = 0;
+	public var height(default,null) = 0;
+	public var overlapPx = 2;
+
+	public function new(?p) {
+		super(Assets.tiles.tile, p);
+	}
+
+	public inline function empty() {
+		clear();
+		curX = 0;
+	}
+
+	public function addIcons(iconId:String, n=1) {
+		for(i in 0...n) {
+			var t = Assets.tiles.getTile(iconId);
+			add(curX, 0, t);
+			width = curX + t.iwidth;
+			height = M.imax(height, t.iheight);
+			curX += t.iwidth-overlapPx;
+		}
+	}
+}
