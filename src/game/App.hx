190b52ec030c786b4ff35a61b4c00c0a1a2798f8
Sebastien Benard <github@deepnight.net>
2024-04-17 12:41:39 +0200

    Added keyboard Up to jump binding

--- src/game/App.hx
+++ src/game/App.hx
@@ -251,7 +251,7 @@ class App extends dn.Process {
 		controller.bindKeyboard(MoveRight, [K.RIGHT, K.D]);
 		controller.bindKeyboard(MoveUp, [K.UP, K.Z, K.W]);
 		controller.bindKeyboard(MoveDown, [K.DOWN, K.S]);
-		controller.bindKeyboard(Jump, K.SPACE);
+		controller.bindKeyboard(Jump, [K.SPACE,K.UP]);
 		controller.bindKeyboard(Restart, K.R);
 		controller.bindKeyboard(ScreenshotMode, K.F9);
 		controller.bindKeyboard(Pause, K.P);
