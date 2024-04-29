4816dc2f34399a3522c3429ebed707aedbf6959d
Sebastien Benard <github@deepnight.net>
2024-04-29 17:04:05 +0200

    Added doc

--- src/game/ui/UiGroupController.hx
+++ src/game/ui/UiGroupController.hx
@@ -7,7 +7,18 @@ enum abstract GroupDir(Int) {
 	var West;
 }
 
+/**
+	This process takes care of interactions with a group of UiComponents.
+	This includes:
+	- focus/blur on a component,
+	- user interaction with a component,
+	- supports gamepad, keyboard and mouse.
 
+	USAGE:
+	- Add some UiComponents to your scene,
+	- Create a UiGroupController instance,
+	- Register the UiComponents in the UiGroupController.
+**/
 class UiGroupController extends dn.Process {
 	var uid : Int;
 	var ca : ControllerAccess<GameAction>;
